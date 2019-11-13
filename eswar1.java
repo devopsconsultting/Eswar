470fcb7eec2dad8d32613a4aafe7a35a056b4b82
simplyesu <simplyesu@gmail.com>
2019-11-13 08:27:52 +0530

    this is going to my commit

---
+++ eswar1.java
@@ -0,0 +1,11 @@
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+System.out.println("Eswar");
+System.out.println("hello besant");
+System.out.println("hello friends");
+  }
+}
